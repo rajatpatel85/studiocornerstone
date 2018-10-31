5b402f2ec089ad19e3974afd873eda6330b8dcf3
Rajat.Patel <Rajat.Patel@wholefoods.com>
2018-10-31 15:51:35 -0500

    code changes for header, Navigation tab and images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './jsx/Home';
+import BrowserDetection from 'react-browser-detection'; 
 
-class App extends Component {
+class App extends Component { 
+
   render() {
+    const browserHandler = {
+      // chrome: () => <div>Chrome is fantastic!</div>,
+      // googlebot: () => <div>Hi GoogleBot!</div>,
+      // default: (browser) => <div>Hi {browser}!</div>,
+      chrome: () =><Home/>,
+      firefox: () => <Home/>,      
+      googlebot: () => <div>Hi GoogleBot!</div>,
+      default: (browser) => <div>Hi {browser}!</div>,
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App"> 
+      <BrowserDetection>
+        { browserHandler }
+      </BrowserDetection>        
       </div>
     );
   }
